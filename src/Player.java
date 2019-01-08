22f34756ee7c8eb4d4c79bed63cae772f240f113
Tehila <tehilaschechter@gmail.com>
2019-01-08 11:54:19 +0200

    Add Player.java

---
+++ src/Player.java
@@ -0,0 +1,38 @@
+
+public class Player {
+	//private String playerName = "";
+	private int playerNum = 0;
+	private int playerScore = 0;
+	//private int currentPlayer = 1;
+	
+	public Player(int playerNum, int playerScore) {
+		//super();
+		this.playerNum = playerNum;
+		this.playerScore = playerScore;
+	}
+		
+	// Getters and Setters
+	/*public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}*/
+	public int getPlayerNum() {
+		return playerNum;
+	}
+	public void setPlayerNum(int playerNum) {
+		this.playerNum = playerNum;
+	}
+	public int getPlayerScore() {
+		return playerScore;
+	}
+	public void setPlayerScore(int playerScore) {
+		this.playerScore = playerScore;
+	}
+	
+	
+		
+	
+	
+}
